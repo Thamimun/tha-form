46292898a0ec8c82a473970dacf72d96ca17750e
Thamimun <thamimum.a@simbiotikgroup.com>
2022-05-17 16:19:52 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {httpService}from './http.service'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DeatailsComponent } from './deatails.component';
+import { EditComponent } from './edit.component';
+import { FormsModule } from '@angular/forms';
+import {formComponent}from './form.component';
+import {DataService}from './global.service'
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule,HttpClientXsrfModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeatailsComponent,
+    EditComponent,formComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,ReactiveFormsModule,HttpClientModule,RouterModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataService,httpService],
+  bootstrap: [  AppComponent
+  ]
 })
 export class AppModule { }
