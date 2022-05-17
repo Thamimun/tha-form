76968b65fed813a1d7f55379fc14a853174b36c0
Thamimun <thamimum.a@simbiotikgroup.com>
2022-05-17 16:19:37 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'formn';
+
+
+
+export class AppComponent{
+  constructor(public route: Router)
+  { }
+  onSubmit=()=>{
+    
+    this.route.navigate(['/form']); 
+      }
+  
 }
+
