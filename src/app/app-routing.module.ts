d300775fde1ada37dfe54da868fd73de74588194
Thamimun <thamimum.a@simbiotikgroup.com>
2022-05-17 16:19:20 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeatailsComponent } from './deatails.component';
+import {formComponent}from './form.component';
+import { EditComponent } from './edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'details' ,component:DeatailsComponent},
+  {path:'form',component:formComponent},
+  {path:'edit/:id',component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
